63abcae100eb35d48a8d1ba86749bf704f0ac62f
nnatna <skyerkh@gmail.com>
2026-04-02 20:56:07 +0700

    Update Report

--- settings.php
+++ settings.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,14 +11,28 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="./assets/css/layout.css?v=1">
 </head>
+
 <body>
     <?php include 'layout/header.php'; ?>
     <div class="main-wrapper">
         <?php include 'layout/sidebar.php'; ?>
         <main class="content-wrapper">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <div>
+                    <h3 class="fw-bold text-success"><i class="fa-solid fa-gear me-1"></i>Setting</h3>
+                    <p class="text-muted">Manage system settings and configurations</p>
+                </div>
+                <div>
+                    <button type="button" class="btn btn-success rounded-pill fw-bold" onclick="window.print()">
+                        <i class="bi bi-printer-fill me-1"></i> Print Report
+                    </button>
+                </div>
+            </div>
         </main>
     </div>
-    <?php include 'layout/footer.php';?>
+    <?php include 'layout/footer.php'; ?>
 </body>
+
 </html>
+
 </html>
